24e1ce6cdfafbd354f173e188d94afebeb385ede
John Li <johnzli1995@gmail.com>
2015-10-09 14:45:07 -0700

    Started with a small template maybe?

---
+++ src/classes.py
@@ -0,0 +1,20 @@
+#TODO: Add some stuff to the classes...
+
+class Device:
+	def __init__(self):
+
+class Router(Device):
+	def __init__(self):
+
+class Host(Device):
+	def __init__(self):
+
+class Flow:
+	def __init__(self):
+
+class Link:
+	def __init__(self):
+
+class Packet:
+	def __init__(self):
+		
---
+++ src/network.py
@@ -0,0 +1,8 @@
+from classes.py import *
+
+#TODO: Implement Bellman-Ford algorithm
+def bellmanFord():
+
+
+if __name__ == "__main__":
+
